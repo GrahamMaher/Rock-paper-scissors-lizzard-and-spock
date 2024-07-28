ebd3f9a998d5075f97c5020a15bd2fa1e956a27d
Graham Maher <mahergrahamsj@gmail.com>
2024-07-28 12:49:47 +0000

    Add base functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,25 @@ const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
 let playerScore = 0;
 let opponentScore = 0;
 
+// Add event listener for button elements //
 document.querySelector('select').forEach(button => {
     button.addEventListener('click', () => {
         playGame(button.id);
     });
 });
+
+function getOpponentChoice(){
+
+}
+
+function getResult(){
+
+}
+
+function updateScores(){
+
+}
+
+function displayMessage(){
+    
+}
