7a975f0a390900a46cbbd91ab79ce52b77d40d4a
Graham Maher <mahergrahamsj@gmail.com>
2024-07-28 13:18:09 +0000

    Add main playGame function and getOpponents choice function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,26 @@ const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
 let playerScore = 0;
 let opponentScore = 0;
 
-// Add event listener for button elements //
+//  Add event listener for button elements //
 document.querySelector('select').forEach(button => {
     button.addEventListener('click', () => {
         playGame(button.id);
     });
 });
+ // functioin calls players choice and the opponents choice to display a result//
+function playGame(playerChoice){
+    //calls random opponents choice//
+    const opponentChoice = getOpponentChoice();
+    //compare players choice to opponents choice to give a result//
+    const result = getResult(playerChoice, opponentChoice);
+    //function updates the score based on the result//
+    updateScores(result);
+    displayMessage(`You chose ${playerChoice}, opponent chose ${opponentChoice}. ${result}`);
+}
 
 function getOpponentChoice(){
-
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
 }
 
 function getResult(){
@@ -22,5 +33,5 @@ function updateScores(){
 }
 
 function displayMessage(){
-    
+
 }
